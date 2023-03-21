5676eb4f7e07f339b027bdb8eb87a9498a2306d4
Anwar <anwarjahid449@gmail.com>
2023-03-21 18:30:18 +0600

    object display dynamically

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const friends =[
+    {name:'sakib', age: 22},
+    {name:'sakib', age: 22},
+    {name:'galib', age: 22},
+    {name:'rakib', age: 24}
+  ]
+  
   return (
     <div className="App">
-
+      {
+        friends.map(friend => <Friend name={friend.name} age={friend.age}></Friend>)
+      }
+      {/* <Friend name='Anwar Jahid'></Friend>
+      <Friend name='sakib khan'></Friend>
+      <Friend></Friend>
+      <Friend></Friend> */}
     </div>
   );
+}
+
+function Friend(props){
+  return (
+    <div className='container'>
+      <h2>{props.name}</h2>
+      <h2>{props.age}</h2>
+    </div>
+  )
+
 }
 
 export default App;
